a129f1cddabdba9479a075e890c52ffec214163b
jason <bintasong@gmail.com>
2017-08-28 20:12:56 +0800

    save

--- sophos/src/medium_storage_sophos_client.hpp
+++ sophos/src/medium_storage_sophos_client.hpp
@@ -52,7 +52,7 @@ namespace sse {
             SearchRequest   search_request(const std::string &keyword) const;
             SearchRequest   search_request_with_counter(const std::string &keyword, const uint32_t kw_counter) const;            
             UpdateRequest   update_request(const std::string &keyword, const index_type index);
-            UpdateRequest   update_request_with_counter(const std::string &keyword, const index_type index, uint32_t &kw_counter_);// added by xiangfu
+            UpdateRequest   update_request_with_counter(const std::string &keyword, const index_type index, uint32_t &kw_counter_); // added by xiangfu
 
             SearchRequest   random_search_request() const;
 
